622f868798bc5ab51ad0d9e95cb984b5515a53b7
Ajay Kotha <32416776+kothaajay@users.noreply.github.com>
2025-06-28 12:31:49 +0530

    Update remove.py

--- Removing unattached EBS Volumes/remove.py
+++ Removing unattached EBS Volumes/remove.py
@@ -3,7 +3,16 @@ import boto3
 ec2 = boto3.client('ec2')
 
 #Get all the volumes in AWS account
-response = ec2.describe_volumes(OwnerIds=['self'])
+response = ec2.describe_volumes()
 
-for volumes in response
+for volumes in response['Volumes']
+    volume_id = volumes['VolumeId']
+  #check whether volume is attached to ec2 instance
+    if not volumes['Attachments']
+       print(f"Volume {volume_id} is unattached to ec2 instances and it will be deleted")
+       ec2.delete_volumes(volume_id)
+    else
+       instance_id = volumes['Attachments']['InstanceId']
+       print(f"Volume {volume_id} is attached to ec2 instance {instance_id} ")
+    
 
