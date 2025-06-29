3b87660cc71c6f9e07ee17fb9de5413e3ec5ea0f
Ajay Kotha <32416776+kothaajay@users.noreply.github.com>
2025-06-29 09:49:30 +0530

    Update update_server.py

--- Server configuration/update_server.py
+++ Server configuration/update_server.py
@@ -1,1 +1,15 @@
+def server_config(file_path,key,value):
 
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        
+    with open(file_path, 'w') as file:
+         for line in lines:
+             if key in line:
+                 file.write(key + '=' + value + '\n') 
+             else: 
+                file.write(line)
+key_value = "server_conf"
+key = "MAX_CONNECTIONS"
+value = "200"
+server_config(key_value,key,value)
