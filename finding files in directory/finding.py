ea73eebd67fedf4bc1d8a8913cc5577361e4b1d1
Ajay Kotha <32416776+kothaajay@users.noreply.github.com>
2025-06-29 10:18:52 +0530

    Update finding.py

--- finding files in directory/finding.py
+++ finding files in directory/finding.py
@@ -1,1 +1,16 @@
+import os
 
+directory = input("Please enter the directories sepearted by space:").split(' ')
+
+for i in range(len(directory)):
+    try:
+        files = os.listdir(directory[i])
+        print(f"files in {directory[i]}:")  
+        for file in files:
+            print(file)
+    except FileNotFoundError:
+        print(f"Directory {directory[i]} does not exist.") 
+    except PermissionError:
+        print(f"Permission denied for directory {directory[i]}.")           
+    
+    
